7bf97b8be4ab4c2d0d3bfe4ed1ea2709fcf5f70c
VIGNESHSHYAM <126313145+VIGNESHSHYAM@users.noreply.github.com>
2024-09-10 22:18:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.use('/trpc', trpcExpress.createExpressMiddleware({
     router: router_1.appRouter,
     createContext: context_1.createContext,
 }));
-app.listen(4000, () => {
+app.listen(10000, () => {
     console.log('Server is running on http://localhost:4000');
 });
